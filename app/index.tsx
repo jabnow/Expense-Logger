fa912c0f8f5f1ea847a4388aa6d00b9ef7461843
gzhu725 <gloriazhu66@gmail.com>
2024-10-26 17:20:12 -0400

    add tessaract code

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,27 @@
+import React, { useEffect } from "react";
 import { Text, View } from "react-native";
+import Tesseract from "tesseract.js";
+
+
 
 export default function Index() {
+  useEffect(() => {
+    const recognizeText = async () => {
+      try {
+        const { data: { text } } = await Tesseract.recognize(
+          '../lol.jpeg', // Provide the correct path to the image
+          'eng',
+          { logger: (m) => console.log(m) }
+        );
+        console.log("Recognized text:", text);
+      } catch (error) {
+        console.error("Error recognizing text:", error);
+      }
+    };
+
+    recognizeText(); // Call the function to initiate OCR
+  }, []);
+
   return (
     <View
       style={{
@@ -9,7 +30,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Edit lll/index.tsx to edit this screen.</Text>
     </View>
   );
 }
